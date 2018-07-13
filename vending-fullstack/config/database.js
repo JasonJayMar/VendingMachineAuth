b2944e8259a07c1a1173f3cd2de3f29248244524
Jason Martinez <39502339+JasonJayMar@users.noreply.github.com>
2018-07-13 14:28:13 -0400

    Add files via upload

---
+++ vending-fullstack/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://vending:vending1@ds233571.mlab.com:33571/vendingmachine' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
